b1685886d3b51c8fb9761f76e25a6445cca3ed8b
Kevin Colyar <kevin@colyar.net>
2017-07-28 16:39:32 -0700

    Adds most to apt packages

--- install_apt.sh
+++ install_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-progs_common="zsh tmux git-core autossh openjdk-7-jre-headless silversearcher-ag"
+progs_common="zsh tmux git-core autossh openjdk-7-jre-headless silversearcher-ag most"
 progs_emacs="emacs-nox silversearcher-ag-el"
 progs_vim="vim-nox"
 progs_dev="build-essential libssl-dev libreadline-dev zlib1g-dev libmysqlclient-dev libsqlite3-dev freetds-dev"
