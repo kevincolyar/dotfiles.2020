805b0c5d44dcf2d45cbfa0c7f3eefd2ce5b405fc
Kevin Colyar <kevinc@dcpud.org>
2018-10-05 15:55:29 -0700

    Adds ncdu

--- install_apt.sh
+++ install_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-progs_common="zsh tmux git-core autossh silversearcher-ag most htop"
+progs_common="zsh tmux git-core autossh silversearcher-ag most htop ncdu"
 progs_emacs="emacs-nox silversearcher-ag-el"
 progs_vim="vim-nox"
 progs_dev="build-essential libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev freetds-dev"
