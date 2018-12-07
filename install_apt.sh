2ec74cf1bfea01718efa287a611c1b8d3fe53928
Kevin Colyar <kevin@colyar.net>
2018-12-07 14:37:42 -0800

    Updates apt install script

--- install_apt.sh
+++ install_apt.sh
@@ -7,6 +7,6 @@ progs_dev="build-essential libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev 
 progs_misc="cmus ranger rxvt-unicode-256color sshfs openvpn"
 progs_servers="openssh-server"
 progs_i3="blueman xinput"
-progs_docs="pandoc texlive-binaries texlive-latex-extra"
+progs_docs="pandoc texlive-binaries texlive-latex-extra mupdf mupdf-tools"
 
 sudo apt-get install -y $progs_common $progs_emacs $prog_vim $progs_dev $progs_servers $progs_misc
