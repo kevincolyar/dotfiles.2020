e74cb10e921f367d55c545d66f6e8118f7e36abc
Kevin Colyar <kevin@colyar.net>
2019-03-04 08:31:57 -0800

    Adds ngrep to apt packages

--- install_apt.sh
+++ install_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-progs_common="zsh tmux git-core autossh silversearcher-ag most htop ncdu urlview"
+progs_common="zsh tmux git-core autossh silversearcher-ag most htop ncdu urlview ngrep"
 progs_emacs="emacs25-nox silversearcher-ag-el"
 progs_vim="vim-nox"
 progs_dev="build-essential libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev freetds-dev"
@@ -8,5 +8,6 @@ progs_misc="cmus ranger rxvt-unicode-256color sshfs openvpn"
 progs_servers="openssh-server"
 progs_i3="blueman xinput"
 progs_docs="pandoc texlive-binaries texlive-latex-extra mupdf mupdf-tools"
+progs_security="keychian"
 
-sudo apt-get install -y $progs_common $progs_emacs $prog_vim $progs_dev $progs_servers $progs_misc
+sudo apt-get install -y $progs_common $progs_emacs $prog_vim $progs_dev $progs_servers $progs_misc $progs_security
